96652cbbdeebfde25d9ae8faba9803435905e19c
bilalkocoglu <bilal.kocoglu@outlook.com.tr>
2018-10-11 17:56:09 +0300

    Hours+3 update

--- src/main/java/com/keyboardforces/ChatService/controller/WebSocketController.java
+++ src/main/java/com/keyboardforces/ChatService/controller/WebSocketController.java
@@ -2,8 +2,6 @@ package com.keyboardforces.ChatService.controller;
 
 import com.google.gson.Gson;
 import com.keyboardforces.ChatService.dto.ChatResponseDTO;
-import com.keyboardforces.ChatService.dto.MessageDTO;
-import com.keyboardforces.ChatService.dto.NativeHeadersDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -12,17 +10,12 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 
-import java.sql.Time;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 @Controller
 public class WebSocketController {
     private final SimpMessagingTemplate simpMessagingTemplate;
-    private final Gson gson = new Gson();
 
     @Autowired
     WebSocketController(SimpMessagingTemplate simpMessagingTemplate){
@@ -31,8 +24,6 @@ public class WebSocketController {
 
     @MessageMapping(value = "/send/message")
     public void onReceivedMessage(@Payload String message, MessageHeaders messageHeaders){
-        Date date = new Date();
-
         LocalDateTime now = LocalDateTime.now();
 
         now = now.plusHours(3);
